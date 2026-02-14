4b32f1f87a97d93a0cf0c309e45696a7d55235ba
sanjay <sanjaythadaka614@gmail.com>
2026-02-14 12:37:41 +0530

    Created User Model

--- server/server.js
+++ server/server.js
@@ -1,2 +1,32 @@
 import express from 'express';
 import cors from 'cors';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+//Express app
+const app = express();
+
+//Middle wares
+app.use(cors());
+app.use(express.json());
+
+//MongoDB connection
+try{
+  mongoose.connect(process.env.MONGO_URI);
+  console.log("Connected to MongoDB");
+} catch (error) {
+  console.error("Error connecting to MongoDB:", error);
+}
+
+//Test route
+app.get('/', (req, res) => {
+  res.send('Welcome to the Event Management API');
+});
+
+//Start the server
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
