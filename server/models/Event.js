bcbefd17bd8736aea4a8ffd224f8caf8b1a20b63
sanjay <sanjaythadaka614@gmail.com>
2026-02-14 12:41:52 +0530

    Created Event Model

---
+++ server/models/Event.js
@@ -0,0 +1,14 @@
+import mongoose from 'mongoose';
+
+const eventSchema = new mongoose.Schema({
+    name: { type: String, required: true },
+    organizer: { type: String, required: true },
+    location: { type: String, required: true },
+    date: { type: Date, required: true },
+    description: { type: String },
+    capacity: { type: Number, required: true },
+    category: { type: String }
+}, { timestamps: true });
+
+const Event = mongoose.model('Event', eventSchema);
+export default Event;
